322d3a956b8f89f4941b55a083799e125b388159
dvbondoy <dvbondoy@yahoo.com>
2025-02-05 17:22:20 +0800

    posts model added

--- connectly_api/urls.py
+++ connectly_api/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter 
-from .views import UserViewSet
+from .views import UserViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
+router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = router.urls
--- connectly_api/views.py
+++ connectly_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Post
+from .serializers import UserSerializer, PostSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('name')
     serializer_class = UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('title')
+    serializer_class = PostSerializer
 
 # Create your views here.
